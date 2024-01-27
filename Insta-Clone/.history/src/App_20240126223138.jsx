6132ad50cc0d2e641f2735621adea3fb97ad58c4
bliuzley13 <arvindkumar.muthukumar@gmail.com>
2024-01-26 22:47:26 -0800

    Project

---
+++ Insta-Clone/.history/src/App_20240126223138.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+    </>
+  );
+}
+
+export default App
